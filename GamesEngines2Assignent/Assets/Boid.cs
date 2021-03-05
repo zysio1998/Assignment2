5fd7aca12cf1e304323e2bf51eecbd4d6b3b591e
Arek Rybicki <zysio1998@gmail.com>
2021-03-05 15:47:53 +0000

    pitched idea done

--- GamesEngines2Assignent/Assets/Boid.cs
+++ GamesEngines2Assignent/Assets/Boid.cs
@@ -78,13 +78,6 @@ public class Boid : MonoBehaviour
     {
         force = Vector3.zero;
 
-        // Weighted prioritised truncated running sum
-        // 1. Behaviours are weighted
-        // 2. Behaviours are prioritised
-        // 3. Truncated
-        // 4. Running sum
-
-
         foreach (SteeringBehaviour b in behaviours)
         {
             if (b.isActiveAndEnabled)
